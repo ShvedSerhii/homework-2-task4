ed28d72ba6bc228b86fc5bb59a8e7ece1c7a41b0
ShvedSerhii <32793438+ShvedSerhii@users.noreply.github.com>
2019-10-18 01:42:06 +0300

    task2

---
+++ script.js
@@ -0,0 +1,61 @@
+let currentDroppable = null;
+let calc = 0;
+let calcElem = document.getElementById("calc");
+
+ball.onmousedown = function(event) {
+  let shiftX = event.clientX - ball.getBoundingClientRect().left;
+  let shiftY = event.clientY - ball.getBoundingClientRect().top;
+
+  ball.style.position = "absolute";
+  ball.style.zIndex = 1000;
+  document.body.append(ball);
+
+  moveAt(event.pageX, event.pageY);
+
+  function moveAt(pageX, pageY) {
+    ball.style.left = pageX - shiftX + "px";
+    ball.style.top = pageY - shiftY + "px";
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+
+    ball.hidden = true;
+    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    ball.hidden = false;
+
+    if (!elemBelow) return;
+
+    let droppableBelow = elemBelow.closest(".droppable");
+    if (currentDroppable != droppableBelow) {
+      if (currentDroppable) {
+        leaveDroppable(currentDroppable);
+      }
+      currentDroppable = droppableBelow;
+      if (currentDroppable) {
+        enterDroppable(currentDroppable);
+      }
+    }
+  }
+
+  document.addEventListener("mousemove", onMouseMove);
+
+  ball.onmouseup = function() {
+    document.removeEventListener("mousemove", onMouseMove);
+    ball.onmouseup = null;
+  };
+};
+
+function enterDroppable(elem) {
+  elem.style.background = "pink";
+  calc++;
+  calcElem.innerHTML = calc;
+}
+
+function leaveDroppable(elem) {
+  elem.style.background = "";
+}
+
+ball.ondragstart = function() {
+  return false;
+};
